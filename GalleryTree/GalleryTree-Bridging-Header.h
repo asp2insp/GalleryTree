0f5b5cc0986faa41a591b4a7e4028c1b6f794db3
David Kuchar <davidkuchar@gmail.com>
2015-05-06 20:59:53 -0700

    we have some awful looking images loading!

---
+++ GalleryTree/GalleryTree-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  GalleryTree-Bridging-Header.h
+//  GalleryTree
+//
+//  Created by David Kuchar on 5/6/15.
+//  Copyright (c) 2015 Josiah Gaskin. All rights reserved.
+//
+
+
+#import "AFNetworking/AFNetworking.h"
+#import "UIImageView+AFNetworking.h"
--- GalleryTree/PhotosViewController.swift
+++ GalleryTree/PhotosViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
-import Alamofire
-
 
 class PhotosViewController: UITableViewController {
 
-    var feed: [String: NSDictionary] = [:]
+    let manager = AFHTTPRequestOperationManager()
+  
+    var feedUrls: [NSURL] = []
+  
+    func extractUrls(feed: NSDictionary) -> [NSURL] {
+        if let feed_data = feed["data"] as? [NSDictionary] {
+          
+          feedUrls = []
+          
+          for image_data: NSDictionary in feed_data {
+              
+              
+              if let feed_images = image_data["images"] as? NSDictionary {
+                if let feed_low_resolution = feed_images["low_resolution"] as? NSDictionary {
+                  if let feed_url = feed_low_resolution["url"] as? String {
+                    
+                    if let url = NSURL(string: feed_url) {
+                      
+                        feedUrls.append(url)
+                    }
+                    
+                    
+                    
+                  }
+                }
+              }
+            }
+          
+          
+        }
+      
+        return feedUrls
+    }
   
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,19 +50,48 @@ class PhotosViewController: UITableViewController {
         // Set the height of each cell
         tableView.rowHeight = 320
       
-        Alamofire.request(.GET, "https://api.instagram.com/v1/media/popular?client_id=\(API_KEY)")
-            .responseJSON { (_, _, JSON, _) in
-                if (JSON != nil) {
-                    self.feed = JSON as! [String: NSDictionary]
-                    self.tableView.reloadData()
+        manager.GET("https://api.instagram.com/v1/media/popular?client_id=\(API_KEY)", parameters: nil, success: { (operation, data) -> Void in
+          
+            println(data)
+          
+            if let feed = data as? NSDictionary {
+                if let feed_meta = feed["meta"] as? NSDictionary {
+                    if feed_meta["code"] as? Int == 200 {
+                      
+                        self.feedUrls = self.extractUrls(feed)
+                        self.tableView.reloadData()
+                    }
                 }
+            }
+          
+        }) { (operation, error) -> Void in
+            println(error.localizedDescription)
         }
+      
+//      
+//        Alamofire.request(.GET, "https://api.instagram.com/v1/media/popular?client_id=\(API_KEY)")
+//            .responseJSON { (_, _, JSON, _) in
+//                if (JSON != nil) {
+//                    self.feed = JSON as! [String: NSDictionary]
+//                    self.tableView.reloadData()
+//                }
+//        }
+      
+      
     }
-    
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return feed["data"]?.count ?? 0
+  
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("instagramPhoto", forIndexPath: indexPath) as! PhotoTableViewCell
+      
+        cell.displayImage.setImageWithURL(self.feedUrls[indexPath.row] ?? nil)
+
+        return cell
     }
 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.feedUrls.count ?? 0
+    }
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
