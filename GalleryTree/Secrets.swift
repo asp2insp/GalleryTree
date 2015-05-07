256f7c4a253ce93ae8e61485d696b1c936c3eedc
Josiah Gaskin <josiahgaskin@gmail.com>
2015-05-06 19:07:54 -0700

    Added alamofire

---
+++ GalleryTree/Secrets.swift
@@ -0,0 +1,10 @@
+//
+//  Secrets.swift
+//  GalleryTree
+//
+//  Created by Josiah Gaskin on 5/6/15.
+//  Copyright (c) 2015 Josiah Gaskin. All rights reserved.
+//
+
+public let API_KEY = "b7c1e41a0f6c41998c2384c797d1ac65"
+public let API_SECRET = "cddd6db409984d75aa253fd6f7723162"
--- GalleryTree/ViewController.swift
+++ GalleryTree/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSLog("KEY: \(API_KEY), SECRET: \(API_SECRET)")
     }
 
     override func didReceiveMemoryWarning() {
